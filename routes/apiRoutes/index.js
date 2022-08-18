988dfdae7859471488e0bfa9918c0536ae0f6d16
Eliot Cleveland <eclevela@richmond.edu>
2022-08-17 21:49:55 -0400

    sve

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./departmentRoutes'));
-// router.use(require('./roleRoutes'));
-// router.use(require('./employeeRoutes'));
+router.use(require('./roleRoutes'));
+router.use(require('./employeeRoutes'));
 
 
 module.exports = router;
