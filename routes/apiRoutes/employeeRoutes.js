fbcccdb19669c1fc4b8e94cab8ce3f40bf5bfdf9
Eliot Cleveland <eclevela@richmond.edu>
2022-08-19 23:06:09 -0400

    employee post route

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
+const inputCheck = require("../../utils/inputCheck");
 
 router.get("/employees", (req, res) => {
   const sql = `SELECT * FROM employee`;
@@ -13,6 +14,40 @@ router.get("/employees", (req, res) => {
     res.json({
       message: "success",
       data: rows,
+    });
+  });
+});
+
+router.post("/employees", ({ body }, res) => {
+  // Candidate is allowed not to be affiliated with a party
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "role_id",
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+  const params = [
+    body.first_name,
+    body.last_name,
+    body.role_id,
+    body.manager_id,
+  ];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+      changes: result.affectedRows,
     });
   });
 });
