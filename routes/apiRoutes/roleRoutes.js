dcd93b32a1f4a118a6c0212e4926ca85fb951b62
Eliot Cleveland <eclevela@richmond.edu>
2022-08-19 22:33:05 -0400

    role post route added

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -1,20 +1,55 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
+const inputCheck = require("../../utils/inputCheck");
 
 router.get("/roles", (req, res) => {
-    const sql = `SELECT * FROM role`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: "success",
-        data: rows,
-      });
+  const sql = `SELECT * FROM role`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
     });
   });
-  
-  module.exports = router;
+});
+
+// Create a candidate
+router.post("/roles", ({ body }, res) => {
+  // Candidate is allowed not to be affiliated with a party
+  const errors = inputCheck(
+    body,
+    "title",
+    "salary",
+    "department_id"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`;
+  const params = [
+    body.title,
+    body.salary,
+    body.department_id,
+  ];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+      changes: result.affectedRows,
+    });
+  });
+});
+
+module.exports = router;
